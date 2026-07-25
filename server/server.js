eda776a9a902bc83112827ba27a4ad8131b413f8
Chaitnyarathod <rathodchaitanya830@gmail.com>
2026-07-25 12:37:52 +0530

    added vercel config

--- server/server.js
+++ server/server.js
@@ -136,6 +136,7 @@ ${message}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+module.exports = app;
